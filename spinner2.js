ec828edce52ee46564c9ddad10a353e9a80b6739
Bita <bita.janzadeh@hotmail.com>
2021-11-30 15:01:06 -0500

    fixed spinner 2

--- spinner2.js
+++ spinner2.js
@@ -1,12 +1,12 @@
 process.stdout.write('hello from spinner1.js... \rheyyy\n');
 
-const ticks = ['|','/','-','|','\\','|',',/','-','\\'];
+const ticks = ['|','/','-','|','\\','|','/','-','\\'];
 let time = 0;
 
 for (const tick of ticks) {
 
   setTimeout(() => {
-    process.stdout.write(`\r + tick`)
+    process.stdout.write(`\r${tick}`)
   }, 100 + time)
   time += 200;
 }
