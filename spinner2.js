ca87576a5698e658ad67afba90566c145a4a92b5
Bita <bita.janzadeh@hotmail.com>
2021-11-30 14:55:00 -0500

    refactored spinner activity into spinner2

---
+++ spinner2.js
@@ -0,0 +1,12 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+const ticks = ['|','/','-','|','\\','|',',/','-','\\'];
+let time = 0;
+
+for (const tick of ticks) {
+
+  setTimeout(() => {
+    process.stdout.write(`\r + tick`)
+  }, 100 + time)
+  time += 200;
+}
